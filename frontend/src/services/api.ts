be2e7a40574f6863b7ad87541422b6a8c10f98d6
Mickael <lorin.mickael@gmail.com>
2026-06-24 09:17:13 +0200

    feat: mise en place partie admin

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,15 @@
+export const getAuthHeaders = () => {
+    const token = localStorage.getItem("accessToken");
+    return {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    };
+};
+
+export const deleteUser = async (id: number) => {
+    const response = await fetch(`${API_URL}/admin/users/${id}`, {
+        method: "DELETE",
+        headers: getAuthHeaders()
+    });
+    return handleResponse(response);
+}
